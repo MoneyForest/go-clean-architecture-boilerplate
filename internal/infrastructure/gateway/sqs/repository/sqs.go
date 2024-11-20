661f728778f465c9d306920fc61d35c558d51bf3
MoneyForest <kanamorishuhei@gmail.com>
2024-11-20 21:07:15 +0900

    fix

--- internal/infrastructure/gateway/sqs/repository/sqs.go
+++ internal/infrastructure/gateway/sqs/repository/sqs.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/MoneyForest/go-clean-boilerplate/internal/infrastructure/gateway/sqs/dto"
 	"github.com/MoneyForest/go-clean-boilerplate/internal/infrastructure/gateway/sqs/entity"
-
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
